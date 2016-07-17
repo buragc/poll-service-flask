fb4dbb362293d03e7ec538590b806bfb3efe7907
Burag Cetinkaya <buragc@gmail.com>
2016-07-17 18:37:27 -0400

    Added a flask-test requirement, wrote simple controller test skeleton.

--- tests/test_poll.py
+++ tests/test_poll.py
@@ -1,3 +1,18 @@
+from poll import create_app
+from flask import jsonify, url_for, json
+import pytest
+
+@pytest.fixture
+def app():
+    app = create_app('testing')
+    return app
+
+
+def testHello(client):
+    result = client.get(url_for('app.hello')).json
+    print("Result is:\n")
+    assert (result['msg'] == 'hello world')
+
 
 def testSilly():
     assert ("12345" == "12345")
